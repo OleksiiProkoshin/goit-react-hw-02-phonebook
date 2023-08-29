e14943abd576d5a15b4bd32a82a961e7ac8ff0cc
OleksiiProkoshin <my.mail2210@gmail.com>
2023-08-29 19:24:49 +0300

    v 1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
+import React, { useState } from 'react';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+import { nanoid } from 'nanoid';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (name, number) => {
+    const newContact = { id: nanoid(), name, number };
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        marginLeft: '30%',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} contacts={contacts} />
+      <h2>Contacts</h2>
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
+      <h3>Find contacts by name</h3>
+      <Filter filter={filter} setFilter={setFilter} />
     </div>
   );
 };
---
+++ src/components/ContactForm.js
@@ -0,0 +1,52 @@
+import { Formik } from 'formik';
+import { StyledField } from './Form.styled';
+import { SubmitButton } from './Form.styled';
+import { StyledForm } from './Form.styled';
+
+export const ContactForm = ({ addContact, contacts }) => {
+  const handleFormSubmit = (values, { resetForm }) => {
+    const isNameTaken = contacts.some(
+      contact => contact.name.toLowerCase() === values.name.toLowerCase()
+    );
+
+    if (isNameTaken) {
+      alert('This name is already in the phonebook.');
+    } else {
+      addContact(values.name, values.number);
+      resetForm();
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      onSubmit={handleFormSubmit}
+    >
+      {({ errors, touched }) => (
+        <StyledForm>
+          <StyledField
+            type="text"
+            name="name"
+            placeholder="Write here your name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          {errors.name && touched.name && <div>{errors.name}</div>}
+
+          <StyledField
+            type="tel"
+            name="number"
+            placeholder="Write here your number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          {errors.number && touched.number && <div>{errors.number}</div>}
+
+          <SubmitButton type="submit">Add Contact</SubmitButton>
+        </StyledForm>
+      )}
+    </Formik>
+  );
+};
